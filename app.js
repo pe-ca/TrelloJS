31edffc8b8a4e429d03017f90ef1ceb3a1a1b734
pe-ca <mr.gloser1@gmail.com>
2022-05-10 13:56:38 -0500

    actualizado app.js

--- app.js
+++ app.js
@@ -3,3 +3,19 @@ require('dotenv').config()
 if(!process.env.TOKEN && !process.env.KEY){
     throw new Error('No hay configuración con Api Key y con Token')
 }
+
+let Trello = require("trello");
+let trello = new Trello(process.env.KEY, process.env.TOKEN);
+
+let cardTitle = `Card Nueva ${new Date()}`
+
+trello.addCard(cardTitle, "LaunchX Card Description", "627aa60781791753c7ac87a1",
+    function (error, trelloCard) {
+        if (error) {
+            console.log('Could not add card:', error);
+        }
+        else {
+            console.log('Added card:', trelloCard);
+        }
+    }
+);
